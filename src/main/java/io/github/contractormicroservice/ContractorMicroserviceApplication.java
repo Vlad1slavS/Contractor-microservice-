45a6aa941db60c4d22de259fc5419d0b7ecd5252
vladstepanov <xstep759@gmail.com>
2025-06-27 01:52:04 +0900

    init commit

---
+++ src/main/java/io/github/contractormicroservice/ContractorMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package io.github.contractormicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractorMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContractorMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/contractormicroservice/ContractorMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.contractormicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractorMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
